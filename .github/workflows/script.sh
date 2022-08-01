c1edbb7d30d7e1414474884e2645aa86ee27609a
Kuntal Ghosh <104074929+KuntalGhoshGM@users.noreply.github.com>
2022-08-01 11:04:00 +0530

    Delete script.sh

--- .github/workflows/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Hi from `hostname`  at `date`..!" >> demo.txt
-sleep5
-echo "Hi from `hostname`  at `date`..!" >> demo.txt
-sleep5
-echo "Hi from `hostname`  at `date`..!" >> demo.txt
